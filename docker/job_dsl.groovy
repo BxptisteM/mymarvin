2277d8f979ac2809e969b75bbe63a2aee42c1473
ThomMASSIN <thom.massin@epitech.eu>
2024-03-12 10:25:20 -0400

    #2 feat: folder Tools is created, and the clone-repository too

--- docker/job_dsl.groovy
+++ docker/job_dsl.groovy
@@ -1,29 +1,20 @@
 folder('Tools') {
     displayName('Tools')
     description('Folder for miscellaneous tools')
+}
 
-    freestyleJob('clone-repository') {
-        displayName('clone-repository')
+freeStyleJob('/Tools/clone-repository') {
+    displayName('clone-repository')
 
-        // Has a GIT_REPOSITORY_URL string parameter with a “Git URL of the repository to clone” description and no default value.
-        parameters {
-            stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
-        }
-        // When executed, clone with Git the repository at the specified URL, using a single shell command.
-        steps {
-            script {
-                sh("git clone \${GIT_REPOSITORY_URL} .")
-            }
-        }
-        // Performs a pre-build workspace cleanup
-        wrappers {
-            preBuildCleanup {
-                preBuildCleanup()
-            }
-        }
-        // Is only executed manually.
-        triggers {
-            manual()
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+    steps {
+        shell("git clone \${GIT_REPOSITORY_URL} .")
+    }
+    wrappers {
+        preBuildCleanup {
+            preBuildCleanup()
         }
     }
 }
